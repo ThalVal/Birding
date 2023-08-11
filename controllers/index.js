a4dbb28487723312924018cf9fc3c24f59356062
ThalVal <vthalissa@yahoo.com>
2023-08-10 23:52:38 -0700

    separate repo again

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const birdRoutes = require("./api/birdRoutes")
-const frontendRoutes = require("./frontendRoutes")
-
-router.use("/api/birds", birdRoutes)
-router.use("/", frontendRoutes)
-
-module.exports = router
