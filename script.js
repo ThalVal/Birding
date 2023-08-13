35c3e81300580db456801d7749e4881ea479ea9c
Ricardo Ledan <ricoledan@gmail.com>
2023-08-13 18:31:02 -0400

    fetch data from backend api

--- script.js
+++ script.js
@@ -1,91 +1,75 @@
 var app = new function() {
     this.el = document.getElementById('birds');
-  
     this.birds = [];
-  
-    
-    
-    this.FetchAll = function() {
-      var data = '';
-  
-      if (this.birds.length > 0) {
-        for (i = 0; i < this.birds.length; i++) {
-          data += '<tr>';
-          data += '<td>'+(i+1)+". " + this.birds[i] + '</td>';
-          data += '<td><button onclick="app.Edit(' + i + ')"  class="molting">Edit</button></td>';
-          data += '<td><button onclick="app.Delete(' + i + ')"  class="fleeting">Delete</button></td>';
-          data += '</tr>';
+
+    this.FetchAll = async function() {
+        try {
+            let response = await fetch('/api/birds'); // replace with your API endpoint
+            this.birds = await response.json();
+
+            let data = '';
+            for (let bird of this.birds) {
+                data += '<tr>';
+                data += '<td>' + bird.name + '</td>';
+                data += '<td><button onclick="app.Edit(' + bird.id + ')">Edit</button></td>';
+                data += '<td><button onclick="app.Delete(' + bird.id + ')">Delete</button></td>';
+                data += '</tr>';
+            }
+            this.el.innerHTML = data;
+            this.Count(this.birds.length);
+        } catch (error) {
+            console.error("Error fetching birds:", error);
         }
-      }
-  
-      this.Count(this.birds.length);
-      return this.el.innerHTML = data;
     };
-  
-    this.Add = function () {
-      el = document.getElementById('watching');
-      // Get the value
-      var bird = el.value;
-  
-      if (bird) {
-        // Add the new value
-        this.birds.push(bird.trim());
-        // Reset input value
-        el.value = '';
-        // Dislay the new list
-        this.FetchAll();
-      }
-    };
-  
-    this.Edit = function (item) {
-      var el = document.getElementById('edit-watching');
-      // Display value in the field
-      el.value = this.birds[item];
-      // Display fields
-      document.getElementById('editz').style.display = 'block';
-      self = this;
-  
-      document.getElementById('hatching btn').onsubmit = function() {
-        // Get value
-        var bird = el.value;
-  
-        if (birds) {
-          // Edit value
-          self.birds.splice(item, 1, bird.trim());
-          // Display the new list
-          self.FetchAll();
-          // Hide fields
-          CloseInput();
+
+    this.Add = async function() {
+        const birdName = document.getElementById('watching').value;
+        if (birdName) {
+            try {
+                await fetch('/api/birds', { // replace with your API endpoint
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ name: birdName })
+                });
+                this.FetchAll();
+            } catch (error) {
+                console.error("Error adding bird:", error);
+            }
         }
-      }
     };
-  
-    this.Delete = function (item) {
-      // Delete the current row
-      this.birds.splice(item, 1);
-      // Display the new list
-      this.FetchAll();
+
+    this.Edit = async function(id) {
+        // Here, I assume you'd fetch the specific bird, edit it and then send an update.
+        // This is a bit more involved than other operations.
+        // As a starter, here's how you'd fetch a specific bird:
+        const bird = this.birds.find(b => b.id === id);
+        if (bird) {
+            // Code to edit and send the update would go here
+        }
     };
-  
+
+    this.Delete = async function(id) {
+        try {
+            await fetch('/api/birds/' + id, { // replace with your API endpoint
+                method: 'DELETE'
+            });
+            this.FetchAll();
+        } catch (error) {
+            console.error("Error deleting bird:", error);
+        }
+    };
+
     this.Count = function(data) {
-      var el   = document.getElementById('counter');
-      var name = 'Birds';
-  
-      if (data) {
-          if(data ==1){
-              name = 'Bird'
-          }
-        el.innerHTML = data + ' ' + name ;
-      } 
-      else {
-        el.innerHTML = 'No ' + name;
-      }
+        var el = document.getElementById('counter');
+        var name = 'Birds';
+        if (data) {
+            if (data == 1) name = 'Bird';
+            el.innerHTML = data + ' ' + name;
+        } else {
+            el.innerHTML = 'No ' + name;
+        }
     };
-    
-  }
-  
-  app.FetchAll();
-  
-  function CloseInput() {
-    document.getElementById('editz').style.display = 'none';
-  }
+
+};
+
+app.FetchAll();
